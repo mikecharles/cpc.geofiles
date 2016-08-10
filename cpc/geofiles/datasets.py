0ede00221af6a48a73a409620b3d51695a28a450
Mike Charles <mike.charles@noaa.gov>
2016-08-10 18:55:57 -0400

    Add datasets.py
    You can create any type of Dataset with datasets.py:
    - Dataset is the base class
    - Forecast is a Dataset with a data_type of 'forecast'
    - EnsembleForecast is a type of Forecast with ensemble information
    - DeterministicForecast is a type of Forecast with regular forecast information, and no ensemble information
    - Climatology is a Dataset with a data_type of 'climatology'
    - Observation is a Dataset with a data_type of 'observation'

---
+++ cpc/geofiles/datasets.py
@@ -0,0 +1,60 @@
+"""
+Defines a Dataset object. Datasets contain one or more data arrays, QC info, etc.
+"""
+
+
+class Dataset:
+    """
+    Base Dataset class
+    """
+    def __init__(self, data_type=None):
+        self.data_type = data_type
+
+
+class Observation(Dataset):
+    """
+    Observation Dataset
+    """
+    def __init__(self, obs=None):
+        Dataset.__init__(self, data_type='observation')
+        self.data_type = 'observation'
+        self.obs = obs
+
+
+class Forecast(Dataset):
+    """
+    Forecast Dataset
+    """
+    def __init__(self):
+        Dataset.__init__(self, data_type='forecast')
+        self.data_type = 'forecast'
+
+
+class EnsembleForecast(Forecast):
+    """
+    Ensemble Forecast Dataset
+    """
+    def __init__(self, ens=None, ens_mean=None, ens_spread=None):
+        Forecast.__init__(self)
+        self.ens = ens
+        self.ens_mean = ens_mean
+        self.ens_spread = ens_spread
+
+
+class DeterministicForecast(Forecast):
+    """
+    Ensemble Forecast Dataset
+    """
+    def __init__(self, fcst=None):
+        Forecast.__init__(self)
+        self.fcst = fcst
+
+
+class Climatology(Dataset):
+    """
+    Climatology Dataset
+    """
+    def __init__(self, clim=None):
+        Dataset.__init__(self, data_type='climatology')
+        self.data_type = 'climatology'
+        self.clim = clim
