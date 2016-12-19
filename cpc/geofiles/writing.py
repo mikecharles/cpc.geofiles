0f31e47916c0ba8c2cd05a1217c315c37d91fb96
Mike Charles <mike.charles@noaa.gov>
2016-12-19 11:59:02 -0500

    Add grd_terciles_to_txt function
    Originally from `data-utils`

--- cpc/geofiles/writing.py
+++ cpc/geofiles/writing.py
@@ -43,3 +43,43 @@ def stn_terciles_to_txt(below, near, above, stn_ids, output_file, in_missing_val
             )
     # Close output file
     f.close()
+
+
+def grd_terciles_to_txt(below, near, above, grid, output_file, missing_val=None):
+    # Reshape data from one to two dimensions
+    if below.ndim == near.ndim == above.ndim == 1:
+        below = np.reshape(below, (grid.num_y, grid.num_x))
+        near = np.reshape(near, (grid.num_y, grid.num_x))
+        above = np.reshape(above, (grid.num_y, grid.num_x))
+    # Open output file
+    f = open(output_file, 'w')
+    # Loop over all grid points
+    f.write('XXYY   below    near   above\n'.format())
+    for x in range(grid.num_x):
+        for y in range(grid.num_y):
+            # TODO: Make the num X and Y sizes dynamic - ex. XXYY vs XXXYYY
+            # If below, near, and above are equal to the missing value
+            # specified, do not format them (leave them as is)
+            if missing_val is not None and \
+                    all([x == missing_val for x in[below[y, x],
+                                                   near[y, x],
+                                                   above[y, x]]]):
+                f.write(
+                    '{:02.0f}{:02.0f}    {:4s}    {:4s}    {:4s}\n'.format(
+                        x + 1,
+                        y + 1,
+                        str(missing_val), str(missing_val), str(missing_val)
+                    )
+                )
+            # If below, near, and above are not equal to the missing value
+            # specified, proceed with formatting them as floats
+            else:
+                f.write(
+                    '{:02.0f}{:02.0f}   {:4.3f}   {:4.3f}   {:4.3f}\n'.format(
+                        x+1, y+1,
+                        below[y, x],
+                        near[y, x],
+                        above[y, x])
+                )
+    # Close output file
+    f.close()
