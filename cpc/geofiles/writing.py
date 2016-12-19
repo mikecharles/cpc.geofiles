3e6893212f4985358d0786407daa27f18d374aa3
Mike Charles <mike.charles@noaa.gov>
2016-12-19 14:54:13 -0500

    Switch order of missing val check
    When the code checks if `in_missing_val` is a NaN, it uses `np.isnan`, which fails when the input is `None`. Therefore the code should instead check if is `None` first, then check if it's a NaN.

--- cpc/geofiles/writing.py
+++ cpc/geofiles/writing.py
@@ -23,10 +23,10 @@ def stn_terciles_to_txt(below, near, above, stn_ids, output_file, in_missing_val
     for i in range(len(stn_ids)):
         # If below, near, and above are equal to the missing value
         # specified, do not format them (leave them as is)
-        if np.isnan(in_missing_val):
-            is_missing = all([np.isnan(x) for x in [below[i], near[i], above[i]]])
-        elif in_missing_val is None:
+        if in_missing_val is None:
             is_missing = all([x is None for x in [below[i], near[i], above[i]]])
+        elif np.isnan(in_missing_val):
+            is_missing = all([np.isnan(x) for x in [below[i], near[i], above[i]]])
         else:
             is_missing = all([x == in_missing_val for x in [below[i], near[i], above[i]]])
         if is_missing:
