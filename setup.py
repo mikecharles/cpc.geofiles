87d5dc96a44fd1cafee3a99ba6e60c957d68ba91
Mike Charles <mike.e.charles@gmail.com>
2019-03-20 09:59:39 -0400

    If fhr_stat is None (eg. wmax) don't do any reshaping

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ requirements = ['cpc.geogrids', 'numpy', 'cpc.units', 'jinja2']
 
 setup(
     name="cpc.geofiles",
-    version='v0.4',
+    version='v0.4.1',
     description="CPC geospatial file interaction (reading, writing, etc.)",
     long_description=readme + '\n\n' + history,
     author="Mike Charles",
